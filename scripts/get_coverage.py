9d38460a7e75209dc7fd5b70c5e8f0b165b317a9
rhysnewell <rhys.newell@uqconnect.edu.au>
2020-06-11 15:17:59 +1000

    Adding in environment yamls and Snakefile

---
+++ scripts/get_coverage.py
@@ -0,0 +1,54 @@
+import subprocess
+import os
+import sys
+
+try:
+    os.makedirs('data/binning_bams')
+except OSError:
+    pass
+already_done = set()
+
+
+if snakemake.config["long_reads"] != "none":
+    subprocess.Popen("coverm contig -t %d -r %s --single %s -p minimap2-ont -m metabat --bam-file-cache-directory data/binning_bams/ > data/long_cov.tsv" %
+                     (snakemake.threads, snakemake.input.fasta, snakemake.config["long_reads"]), shell=True).wait()
+
+if snakemake.config['short_reads_2'] != 'none':
+    subprocess.Popen("coverm contig -t %d -r %s -1 %s -2 %s -m metabat --bam-file-cache-directory data/binning_bams/  > data/short_cov.tsv" %
+                     (snakemake.threads, snakemake.input.fasta, snakemake.config["short_reads_1"], snakemake.config["short_reads_2"]), shell=True).wait()
+
+elif snakemake.config['short_reads_1']  != 'none':
+    subprocess.Popen("coverm contig -t %d -r %s --interleaved %s -m metabat --bam-file-cache-directory data/binning_bams/  > data/short_cov.tsv" %
+                     (snakemake.threads, snakemake.input.fasta, snakemake.config["short_reads_1"]), shell=True).wait()
+
+with open('data/coverm.cov', 'w') as file3:
+    with open('data/short_cov.tsv', 'r') as file1:
+        with open('data/long_cov.tsv', 'r') as file2:
+            for line1, line2 in zip(file1, file2):
+                print(line1.strip(), "\t".join(line2.strip().split()[1::]), file=file3)
+try:
+    os.makedirs("data/maxbin_cov/")
+except OSError:
+    pass
+with open("data/coverm.cov") as f, open("data/maxbin.cov.list", "w") as o:
+    cov_list = []
+    contig_list = []
+    f.readline()
+    for line in f:
+        contig = line.split()[0]
+        contig_list.append(contig)
+        depths = line.split()[3:]
+        cov_list.append([])
+        for i in range(0, len(depths), 2):
+            cov_list[-1].append(depths[i])
+    for i in range(len(cov_list[0])):
+        with open("data/maxbin_cov/p%d.cov" % i, 'w') as oo:
+            for j in range(len(contig_list)):
+                oo.write(contig_list[j] + '\t' + cov_list[j][i] + '\n')
+        o.write("data/maxbin_cov/p%d.cov\n" % i)
+with open("data/binning_bams/done", 'w') as o:
+    o.write('done')
+
+
+
+
