8f7a132ba1594ddb7d1018d2bdba93af7359ee25
Rhys Newell <rhys.newell@hdr.qut.edu.au>
2021-02-05 10:36:56 +1000

    fixing the singlem pplcare threads

--- aviary/scripts/singlem_reads.py
+++ aviary/scripts/singlem_reads.py
@@ -10,19 +10,19 @@ except OSError:
 if snakemake.config["long_reads"] != "none":
         subprocess.Popen(
             "singlem pipe -t %d --sequences %s --otu_table data/singlem_out/metagenome.longread_otu_table.csv" %
-            (snakemake.pplacer_threads, " ".join(snakemake.config["long_reads"])), shell=True).wait()
+            (snakemake.config["pplacer_threads"], " ".join(snakemake.config["long_reads"])), shell=True).wait()
 
 
 if snakemake.config["short_reads_2"] != "none":
         subprocess.Popen(
             "singlem pipe -t %d --forward %s --reverse %s --otu_table data/singlem_out/metagenome.shortread_otu_table.csv" %
-            (snakemake.pplacer_threads,
+            (snakemake.config["pplacer_threads"],
              " ".join(snakemake.config["short_reads_1"]),
              " ".join(snakemake.config["short_reads_2"])), shell=True).wait()
 elif snakemake.config["short_reads_1"] != "none":
         subprocess.Popen(
             "singlem pipe -t %d --sequences %s --otu_table data/singlem_out/metagenome.shortread_otu_table.csv" %
-            (snakemake.pplacer_threads,
+            (snakemake.config["pplacer_threads"],
              " ".join(snakemake.config["short_reads_1"])), shell=True).wait()
 
 subprocess.Popen(
