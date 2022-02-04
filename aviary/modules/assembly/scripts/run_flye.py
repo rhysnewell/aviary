5d3ec6c5d237ac7c6fd5c05e9e66d5bfc5b1ce62
julianzaugg <julianzaugg@gmail.com>
2022-02-04 11:41:52 +1000

    Option to provide high quality nanopore reads to flye with --nano-hq. Default to --nano-raw.

--- aviary/modules/assembly/scripts/run_flye.py
+++ aviary/modules/assembly/scripts/run_flye.py
@@ -3,6 +3,10 @@ import subprocess
 
 if snakemake.params.long_read_type == 'ont':
     subprocess.Popen(
+        "flye --nano-raw %s --meta -o data/flye -t %d -g %d" %
+        (snakemake.input.fastq, snakemake.threads, snakemake.params.genome_size), shell=True).wait()
+elif snakemake.params.long_read_type == 'ont_hq':
+    subprocess.Popen(
         "flye --nano-hq %s --meta -o data/flye -t %d -g %d" %
         (snakemake.input.fastq, snakemake.threads, snakemake.params.genome_size), shell=True).wait()
 elif snakemake.params.long_read_type == 'ccs':
